01df02fba4c4a7d486217c786c2252e98cbafc64
Tyler Tanner <tyler.j.tanner2@gmail.com>
2022-07-28 15:46:58 -0400

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Tyler Tanner";
+    int age = 22;
+    String state = "Wisconsin";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
