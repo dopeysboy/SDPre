57fd676b0610db5cd68162330fbe160416514f45
Tyler Tanner <tyler.j.tanner2@gmail.com>
2022-07-28 15:50:47 -0400

    Completed Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args){
+    int year = 2000;
+    String month = "March";
+    boolean weekend = false;
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}
